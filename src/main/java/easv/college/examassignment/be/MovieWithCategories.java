222b033844aae6c95e12edc57cf178c0c49b1956
SallyyO <salols@protonmail.com>
2026-01-08 12:22:16 +0100

    Made a temp BE, idk if we will use it in the future

---
+++ src/main/java/easv/college/examassignment/be/MovieWithCategories.java
@@ -0,0 +1,23 @@
+package easv.college.examassignment.be;
+
+public class MovieWithCategories {
+    private Movie movie;
+    private String categoriesString; // Comma-separated category names
+
+    public MovieWithCategories(Movie movie, String categoriesString) {
+        this.movie = movie;
+        this.categoriesString = categoriesString;
+    }
+
+    public Movie getMovie() {return movie;}
+
+    public void setMovie(Movie movie) {this.movie = movie;}
+
+    public String getCategoriesString() {return categoriesString;}
+
+    public void setCategoriesString(String categoriesString) {this.categoriesString = categoriesString;}
+
+    @Override
+    public String toString() {return movie.getName() + " [" + categoriesString + "]";}
+}
+
