acd0f3aeac80f482f66c08b0b5e1e3e12d22e77d
ConMcg2025 <cjmcgrail2011@hotmail.com>
2026-01-08 11:13:06 +0100

    Updated Logic class and started methods

--- src/main/java/easv/college/examassignment/bll/Logic.java
+++ src/main/java/easv/college/examassignment/bll/Logic.java
@@ -1,4 +1,70 @@
 package easv.college.examassignment.bll;
 
-public class Logic {
+import easv.college.examassignment.dal.CatMovieDAO;
+import easv.college.examassignment.be.*;
+import java.util.List;
+
+public class Logic
+{
+    private final CatMovieDAO dao;
+
+    public Logic()
+    {
+        dao = new CatMovieDAO();
+    }
+
+    public List<Movie> getAllMovies() throws Exception
+    {
+        return null;
+    }
+
+    public void createMovie(Movie movie) throws Exception
+    {
+
+    }
+
+    public void updateMovie(Movie movie) throws Exception
+    {
+
+    }
+
+    public void deleteMovie(Movie movie) throws Exception
+    {
+
+    }
+
+    public List<Category> getAllCategories() throws Exception
+    {
+        return null;
+    }
+
+    public void createCategory(Category category) throws Exception
+    {
+
+    }
+
+    public void updateCategory(Category category) throws Exception
+    {
+
+    }
+
+    public void deleteCategory(Category category) throws Exception
+    {
+
+    }
+
+    public List<Movie> filterMovies(String query) throws Exception
+    {
+        return null;
+    }
+
+    public List<Category> filterCategory(Category category) throws Exception
+    {
+        return null;
+    }
+
+    public void addCategoryToMovie(Category category) throws Exception
+    {
+
+    }
 }
