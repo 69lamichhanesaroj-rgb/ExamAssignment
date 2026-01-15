03e18978d1e5f89497c8ac734a83b077ea5249ec
ConMcg2025 <cjmcgrail2011@hotmail.com>
2026-01-15 10:42:27 +0100

    removed unused imports and fxml in category controller

--- src/main/java/easv/college/examassignment/gui/AddCategoryController.java
+++ src/main/java/easv/college/examassignment/gui/AddCategoryController.java
@@ -2,18 +2,13 @@ package easv.college.examassignment.gui;
 
 import easv.college.examassignment.be.Category;
 import easv.college.examassignment.bll.Logic;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AddCategoryController {
     Logic logic = new Logic();
@@ -23,9 +18,6 @@ public class AddCategoryController {
 
     @FXML
     private TextField categoryName;
-
-    @FXML
-    private Button saveBtn;
 
     public void btnCancelAction(ActionEvent event) {
         Stage stage = (Stage) cancelBtn.getScene().getWindow();
